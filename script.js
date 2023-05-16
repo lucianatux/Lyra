2c96b00d62f67f60acfc4987068d3751c845de17
lucianatux <lucianatux@yahoo.com.ar>
2023-05-15 21:13:14 -0300

    carousel

---
+++ script.js
@@ -0,0 +1,15 @@
+/*
+let carousel = document.getElementById('carouselExampleIndicators');
+let pausePlayButton = document.getElementById('pausePlayButton');
+let isPaused = false;
+
+  pausePlayButton.addEventListener('click', function() {
+    if (isPaused) {
+      carousel.carousel('cycle'); // Reanuda el carrusel
+      isPaused = false;
+    } else {
+      carousel.carousel('pause'); // Pausa el carrusel
+      isPaused = true;
+    }
+  });
+  */
