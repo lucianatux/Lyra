8a9f0fe20371591a5380f74426a9e74f23cdc8dd
lucianatux <lucianatux@yahoo.com.ar>
2024-01-11 16:49:47 -0300

    fotos

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 function toggleDarkMode() {
   const body = document.body;
   const img= document.getElementById('lightdark');
-  if (body.classList.contains('dark-mode')) {
-    body.classList.remove('dark-mode');
-    body.classList.add('light-mode');
+  if (body.classList.contains('light-mode')) {
+    body.classList.remove('light-mode');
+    body.classList.add('dark-mode');
     img.style.filter = ''; 
 
   } else {
-    body.classList.remove('light-mode');
-    body.classList.add('dark-mode');
+    body.classList.remove('dark-mode');
+    body.classList.add('light-mode');
     img.style.filter = 'invert(100%)'; 
   }
 }
