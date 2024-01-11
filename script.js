180064c2c91f045467da5f36d3da6245b2ec29ce
lucianatux <lucianatux@yahoo.com.ar>
2024-01-10 21:18:21 -0300

    libreria

--- script.js
+++ script.js
@@ -12,3 +12,17 @@ function toggleDarkMode() {
     img.style.filter = 'invert(100%)'; 
   }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Obtener elementos relevantes
+  var listGroup = document.querySelector('.list-group');
+  var imageDisplay = document.getElementById('image-display');
+
+  // Agregar un evento a todos los elementos <p> dentro de .list-group
+  listGroup.addEventListener('mouseover', function (event) {
+    if (event.target.tagName === 'P') {
+      var imageUrl = event.target.getAttribute('data-image');
+      imageDisplay.src = imageUrl;
+    }
+  });
+});
